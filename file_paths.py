6e2fb5c3cd8d158e68158b197ad83f239e2b574b
chandrajyothib <123759115+chandrajyothib@users.noreply.github.com>
2024-01-30 10:55:49 +0530

    file_paths.py

--- file_paths.py
+++ file_paths.py
@@ -1,62 +1,73 @@
 #--------file paths------
 
-# import os
+import os
 
-# directory =input("Input :")
-# print("Output :")
+directory =input("Input :")
+print("Output :")
 
-# for root, dir, files in os.walk(directory):
-#     for file in files:
-#         file_path = os.path.join(root,file)
-#         print(file_path)
+for root, dir, files in os.walk(directory):
+    for file in files:
+        file_path = os.path.join(root,file)
+        print(file_path)
 
 
 #--------count word frequency in csv file---
 
-# import pandas as pd
-# from collections import Counter
-
-# def count_word(file_path):
-#     try:
-#         data = pd.read_csv(file_path)
-#         text = " ".join(data.to_string(index=False).split())
-#         word_counts = Counter(text.lower().split())
-#         return word_counts
-    
-#     except Exception as e:
-#         print(f"Error processing {file_path}: {e}")
-#         return None
-
-# csv_file = "/home/chandu/Desktop/sample.csv"
-# word_frequencies = count_word(csv_file)
-
-# if word_frequencies:
-#     print("\nWord frequencies in the CSV file:")
-#     for word, count in word_frequencies.most_common():
-#         print(f"{word}: {count}")
-
-
-
-#--------count word frequency in excel file---
-
 import pandas as pd
-from collections import Counter
 
-def count_word_xlsx(file_path):
+def count_word(file_path):
     try:
-        data = pd.read_excel(file_path)
-        text = " ".join(data.to_string(index=False).split())
-        word_counts = Counter(text.lower().split())
-        return word_counts
-
+        data = pd.read_csv(file_path)
+        s=" "
+        s= s.join(data.to_string(index=False).split())
+        dict1={}
+        words=s.lower().split()
+        for word in words:
+            if word in dict1:
+                dict1[word]+=1
+            else:
+                dict1[word]=1
+        
+        return sorted(dict1.items(),key=lambda x:x[1],reverse=True)
+    
     except Exception as e:
         print(f"Error processing {file_path}: {e}")
         return None
 
-xlsx_file = "/home/chandu/anaconda3/FINAL450.xlsx"
-word_frequencies = count_word_xlsx(xlsx_file)
+csv_file = "/home/chandu/Desktop/sample.csv"
+word= count_word(csv_file)
+
+if word:
+    print("\nWord frequencies in the CSV file:")
+    print(word)
+
+
+
+#--------count word frequency in excel file---
+import pandas as pd
+
+def count_word(file_path):
+    try:
+        data = pd.read_excel(file_path)
+        s=" "
+        s= s.join(data.to_string(index=False).split())
+        dict1={}
+        words=s.lower().split()
+        for word in words:
+            if word in dict1:
+                dict1[word]+=1
+            else:
+                dict1[word]=1
+        
+        return sorted(dict1.items(),key=lambda x:x[1],reverse=True)
+    
+    except Exception as e:
+        print(f"Error processing {file_path}: {e}")
+        return None
+
+csv_file = "FINAL450.xlsx"
+word_frequencies = count_word(csv_file)
 
 if word_frequencies:
-    print("\nWord frequencies in the XLSX file:")
-    for word, count in word_frequencies.most_common():
-        print(f"{word}: {count}")
+    print("\nWord frequencies in the CSV file:")
+    print(word_frequencies)
