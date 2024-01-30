b859601d41f5ac33fad92698192256e8264a3b3b
chandrajyothib <123759115+chandrajyothib@users.noreply.github.com>
2024-01-30 11:25:37 +0530

    Update file_paths.py

--- Task1/file_paths.py
+++ Task1/file_paths.py
@@ -1,45 +1,41 @@
 #--------file paths------
 
-# import os
+import os
 
-# directory =input("Input :")
-# print("Output :")
+directory =input("Input :")
+print("Output :")
 
-# for root, dir, files in os.walk(directory):
-#     for file in files:
-#         #file_path = os.path.join(root,file)
-#         print(root,file)
+for root, dir, files in os.walk(directory):
+    for file in files:
+        #file_path = os.path.join(root,file)
+        print(root,file)
 
 
 #--------count word frequency in csv file---
 
-# import pandas as pd
+import pandas as pd
 
-# def count_word(file_path):
-#     try:
-#         data = pd.read_csv(file_path)
-#         s=" "
-#         s= s.join(data.to_string(index=False).split())
-#         dict1={}
-#         words=s.lower().split()
-#         for word in words:
-#             if word in dict1:
-#                 dict1[word]+=1
-#             else:
-#                 dict1[word]=1
+def count_word(file_path):
+    try:
+        data = pd.read_csv(file_path)
+        s=" "
+        s= s.join(data.to_string(index=False).split())
+        dict1={}
+        words=s.lower().split()
+        for word in words:
+            if word in dict1:
+                dict1[word]+=1
+            else:
+                dict1[word]=1
         
-#         return sorted(dict1.items(),key=lambda x:x[1],reverse=True)
+        return sorted(dict1.items(),key=lambda x:x[1],reverse=True)
     
-#     except Exception as e:
-#         print(f"Error processing {file_path}: {e}")
-#         return None
+    except Exception as e:
+        print(f"Error processing {file_path}: {e}")
+        return None
 
-# csv_file = "/home/chandu/Desktop/sample.csv"
-# word= count_word(csv_file)
-
-# if word:
-#     print("\nWord frequencies in the CSV file:")
-#     print(word)
+csv_file = "/home/chandu/Desktop/sample.csv"
+print("\nWord frequencies in the EXCEL file:",count_word(csv_file))
 
 #-------count word frequency in excel files----
 
@@ -64,9 +60,5 @@ def count_word(file_path):
         print(f"Error processing {file_path}: {e}")
         return None
 
-csv_file = "FINAL450.xlsx"
-word_frequencies = count_word(csv_file)
-
-if word_frequencies:
-    print("\nWord frequencies in the CSV file:")
-    print(word_frequencies)
+excel_file = "FINAL450.xlsx"
+print("Word frequency in excel file is as follows :\n"count_word(excel_file))
