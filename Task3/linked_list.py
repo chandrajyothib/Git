12739b8e9dabbd50c30228fdb8f0ebdf06bd8298
chandrajyothib <123759115+chandrajyothib@users.noreply.github.com>
2024-03-01 16:32:50 +0530

    Update linked_list.py

--- Task3/linked_list.py
+++ Task3/linked_list.py
@@ -8,6 +8,9 @@ class node:
 class ll:
     def __init__(self):
         self.head=None
+        
+    #----print Linked List
+    
     def print(self):
         if self.head is None:
             print("LL is empty")
@@ -16,10 +19,13 @@ class ll:
             while n is not None:
                 print(n.data )
                 n=n.next
+    #---add element at begin
     def add_begin(self,data):
         new_node=node(data)
         new_node.next=self.head
         self.head=new_node
+        
+    #---add element at end
     def add_end(self,data):
         new_node=node(data)
         n=self.head
@@ -29,6 +35,8 @@ class ll:
             while n.next is not None:
                 n=n.next
             n.next=new_node
+
+    #---add element at index
     def add_index(self,index,data):
         new_node=node(data)
         if(self.head==None or index==1):
@@ -41,7 +49,7 @@ class ll:
         new_node.next=temp.next
         temp.next=new_node  
             
-    
+    #---delete element at begin
     def del_begin(self):
         if(self.head==None):
             print("LL is empty")
@@ -49,6 +57,8 @@ class ll:
             print("LL is empty")
         n=self.head
         self.head=n.next
+
+    #---delete element at end
     def del_end(self):
         if(self.head==None):
             print("LL is empty")
@@ -58,6 +68,8 @@ class ll:
         while(temp.next.next!=None):
             temp=temp.next
         temp.next=None
+
+    #---delete element at index
     def del_index(self,index):
         if(self.head==None):
             print("LL is empty")
@@ -68,6 +80,8 @@ class ll:
         for i in range(0,index-2):
             temp=temp.next
         temp.next=temp.next.next
+
+    #----update data at given index
     def update(self,index,data):
         if(self.head==None):
             print("LL is empty")
@@ -75,11 +89,8 @@ class ll:
         for i in range(0,index-1):
             temp=temp.next
         temp.data=data
+   
         
-        
-            
-    
-
 n=ll()
 n.add_begin(20)
 n.add_begin(40)
@@ -88,8 +99,9 @@ n.add_begin(100)
 n.add_end(20)
 n.add_index(1,500)
 n.print()
-
-# n.del_index(2)
+n.del_begin()
+n.del_index(2)
+n.del_end()
 print("Updated List: \n")
 n.update(2,30)
 n.print()
